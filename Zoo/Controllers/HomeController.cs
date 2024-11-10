5b318f75967a17d5231cdd32e417ae9d95dbf284
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-10 16:26:55 +0200

    Create and Add content to Home page controller and view

---
+++ Zoo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zoo.Data;
+
+namespace Zoo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var topAnimals = await context.Animals
+            .Include(a => a.Comments)
+            .OrderByDescending(a => a.Comments.Count)
+            .Take(2)
+            .ToListAsync();
+
+            return View(topAnimals);
+        }
+    }
+}
