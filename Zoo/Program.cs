f94b5ca6ec30b80ee6579141c509c473ed65190c
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-10 15:52:28 +0200

    Connect program to DB and install required nuget

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Zoo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
+string connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"]!;
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    ctx.Database.EnsureDeleted();
+    ctx.Database.EnsureCreated();
+}
 
 app.UseStaticFiles();
 app.UseRouting();
