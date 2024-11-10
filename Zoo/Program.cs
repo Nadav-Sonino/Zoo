f4bd4e5766e43b1d0d0c9a8e13f54f06bbd6db36
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-10 15:35:09 +0200

    Init default routing and services for mvc

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-
+app.UseStaticFiles();
+app.UseRouting();
+app.MapDefaultControllerRoute();
 
 app.Run();
