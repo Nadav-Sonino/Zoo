4e31895150a8461acacd2f6cf73c07b2d42afa54
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-10 15:43:56 +0200

    Init Animal, Category and Comment models

---
+++ Zoo/Models/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Zoo.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ Zoo/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Zoo.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+
+        public int AnimalId { get; set; }
+
+        public Animal Animal { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
