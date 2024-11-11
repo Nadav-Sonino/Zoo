0b6c0a340df6a5c7c215b817992aeb144c415ef2
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-11 10:55:49 +0200

    Add Catalog controller and view

---
+++ Zoo/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zoo.Data;
+
+namespace Zoo.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CatalogController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var animals = await context.Animals.Include(a => a.Comments).ToListAsync();
+            return View(animals);
+        }
+    }
+}
