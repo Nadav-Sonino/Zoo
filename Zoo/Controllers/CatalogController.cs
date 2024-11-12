7644423fd2ca699271327cab452970a5bfbe117c
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-12 11:49:37 +0200

    Add Comments section in details page

--- Zoo/Controllers/CatalogController.cs
+++ Zoo/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Zoo.Data;
+using Zoo.Models;
 
 namespace Zoo.Controllers
 {
@@ -18,10 +19,42 @@ namespace Zoo.Controllers
             var animals = await context.Animals.Include(a => a.Comments).ToListAsync();
             return View(animals);
         }
+
         public async Task<IActionResult> Details(int id)
         {
-            var animal = await context.Animals.Where(a => a.AnimalId == id).Include(a => a.Comments).Include(a => a.Category).SingleAsync();
+            var animal = await context.Animals.Include(a => a.Category)
+            .Include(a => a.Comments)
+            .FirstOrDefaultAsync(a => a.AnimalId == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
             return View(animal);
+        }
+
+        public async Task<IActionResult> AddComment(int AnimalId, string Content)
+        {
+            var animal = await context.Animals.FindAsync(AnimalId);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            if (Content.Length > 80) 
+            {
+                return RedirectToAction("Details", new { id = AnimalId });
+            }
+            var comment = new Comment
+            {
+                AnimalId = AnimalId,
+                Content = Content,
+            };
+
+            context.Comments.Add(comment);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Details", new { id = AnimalId });
         }
     }
 }
