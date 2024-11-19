7883fb3414ab191f36a4f0a38b7c6c344cadf345
Nadav-Sonino <57793797+Nadav-Sonino@users.noreply.github.com>
2024-11-19 17:54:17 +0200

    Style comments text input

--- Zoo/Controllers/AdminController.cs
+++ Zoo/Controllers/AdminController.cs
@@ -10,6 +10,20 @@ namespace Zoo.Controllers
         {
             return View();
         }
-        
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
